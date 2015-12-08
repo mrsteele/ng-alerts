b2c10b38680ee9a56797dfdf05d3ad86b2ab26a6
Matt Steele <matt@omnionline.us>
2015-12-07 23:43:03 -0500

    Made some adjustments to enhance notification popover mechanics

--- dist/ng-notify.js
+++ dist/ng-notify.js
@@ -109,24 +109,16 @@ angular.module('ngNotify').directive('ngNotifyPopover', [
                 var position = $attrs.position || 'top',
                     $el = $compile('<div class="ng-notify-popover popover fade ' + position + ' in" role="tooltip" id="popover677443" style="top: 25477px; left: 266px; display: block;"><div class="arrow" style="left: 50%;"></div><div class="popover-content"><ng-notify-list></ng-notify-list></div></div>')($scope),
                     body = angular.element(document).find('body');
-
-                function interrupt(e) {
-                    e.stopPropagation();
-                }
                 
                 function hidePopover(e) {
-                    $el.remove();
-                    body.off('click', hidePopover);
+                    $el.detach();
                 }
-
-                function click() {
-                    body.off('click', hidePopover);
+                
+                function positionPopover() {
                     var rect = $element[0].getBoundingClientRect(),
                         top,
                         left;
-
-                    body.append($el);
-
+                    
                     if (position === 'top' || position === 'bottom') {
                         top = (position === 'bottom') ? rect.bottom : rect.top + $element[0].offsetHeight;
                         left = rect.left + ($element[0].offsetWidth / 2) - ($el[0].offsetWidth / 2);
@@ -139,21 +131,33 @@ angular.module('ngNotify').directive('ngNotifyPopover', [
                         top: top,
                         left: left
                     });
+                }
 
-                    // clicking anywhere else deletes the $el
-                    $timeout(function () {
-                        $el.on('click', interrupt);
-                        body.on('click', hidePopover);
-                    });
+                function interrupt(e) {
+                    e.stopPropagation();
+                    positionPopover();
+                }
+
+                function click(e) {
+                    interrupt(e);
+                    hidePopover(e);
+
+                    body.append($el);
+                    
+                    positionPopover();
                 }
 
                 function destroy() {
                     $element.off('click', click);
                     body.off('click', hidePopover);
                     $el.off('click', interrupt);
+                    
+                    $el.remove();
                 }
 
                 $element.on('click', click);
+                body.on('click', hidePopover);
+                $el.on('click', interrupt);
 
                 $scope.$on('$destroy', destroy);
 
